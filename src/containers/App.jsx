eeb9fc231881fb2adc90f37e83fce8f51ef513a1
Ruben Santibanez <santibanez.ruben@uni.eus>
2021-08-18 15:49:50 +0100

    construir componentes iterando con useState haciendo fectch a un fake-API(.json)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -6,42 +6,66 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
+import { useEffect, useState } from "react";
 
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Mi Lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const App = () => {
+    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect(() => {
+        fetch("http://localhost:3000/initialState")
+            .then(response => response.json())
+            .then(data => setVideos(data))
+            .then(console.log(videos))
+            .catch(err => console.error(err))
+    }, []);
 
-        <Categories title="Originales de Platzi Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer/>
-    </div>
-);
+
+
+
+
+    return (
+
+        <div className="App">
+            <Header />
+            <Search />
+            {
+                videos.mylist.length > 0 &&
+                <Categories title="Mi Lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
+            {
+                videos.trends.length > 0 &&
+
+                <Categories title="Tendencias">
+                    <Carousel>
+                        {
+                            videos.trends.map(item =>
+                                <CarouselItem key={item.id} {...item} />
+                            )
+                        }
+
+                    </Carousel>
+                </Categories>
+            }
+            {
+                videos.originals.length > 0 &&
+                <Categories title="Originales de Platzi Video">
+                    <Carousel>
+                    {
+                            videos.originals.map(item =>
+                                <CarouselItem key={item.id} {...item} />
+                            )
+                        }
+                    </Carousel>
+                </Categories>
+            }
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
